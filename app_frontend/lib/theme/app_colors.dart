c5be271dea6dcb79f934e9c167cefbcc51320146
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2025-01-04 11:45:35 +0700

    feat(color-custom): create custom app color

---
+++ app_frontend/lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.blue; // blue
+  static const Color secondaryColor = Color(0xFF03DAC6); // Teal
+  static const Color accentColor = Color(0xFFFF5722); // Orange
+}
